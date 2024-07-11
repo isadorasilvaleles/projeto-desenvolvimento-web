0c83a7536237a3fd8131be7ef3be501dbe8276fc
isadora leles <64217150+isadorasilvaleles@users.noreply.github.com>
2024-07-11 16:53:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const switcher = document.querySelector('btn');
+switcher.addEventListener('click', function() {
+
+document.body.classList.toggle('dark-theme');
+
+
+
+var classname = document.body.className;
+if(className == "light-theme") {
+
+this.textContent = "Dark" ;
+
+}
+else {
+
+    this.textContent = "Light" ;
+
+}
+
+});
+
+console.log('current class name:'+ className);
+
